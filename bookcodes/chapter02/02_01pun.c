feefedcb3f9c8fb85e10abc9b01a337472fed99f
AndyHsu <AndyHsu.CN@gmail.com>
2024-03-30 17:39:56 +0800

    提交第02章示例代码
    数量：4

---
+++ bookcodes/chapter02/02_01pun.c
@@ -0,0 +1,12 @@
+/********************************************************************************
+ * Name: 02_01pun.c                                                             *
+ * Purpose: Prints a bad pun.                                                   *
+ * Author: Andy Hsu                                                             *
+ ********************************************************************************/
+#include <stdio.h>
+
+int main(void)
+{
+    printf("To C, or not to C: that is the question.\n");
+    return 0;
+}
